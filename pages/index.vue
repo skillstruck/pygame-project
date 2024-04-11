eeb0644aa1f900262eb549b08136ef5d87821027
Colleen King <colleenking0810@gmail.com>
2024-04-11 15:02:52 -0600

    build: add pygame code

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,35 @@
 
 <script setup lang="ts">
 
-const currentCode = ref('');
+const currentCode = ref(`# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([400, 400])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()`);
 const showDisplay = ref(true);
 const display = ref();
 const terminal = ref();
